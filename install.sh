3214977d820d9453ecbf987a39ad5d84fb4dc5a2
Max Hill <max.hill@hiq.se>
2017-12-14 13:20:06 +0100

    Add default ackrc
    The default ackrc includes ignores for many folders like
    node_modules. This makes grep alot more clean removing all
    rubbish files.

--- install.sh
+++ install.sh
@@ -18,7 +18,10 @@ ln -s ${BASEDIR}/gitconfig ~/.gitconfig
 ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
 ln -s ${BASEDIR}/teamocil/ ~/.teamocil
 
-#vsCode
+# vsCode
 ln -s ${BASEDIR}/vsCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
 ln -s ${BASEDIR}/vsCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
 ln -s ${BASEDIR}/vsCode/snippets/ ~/Library/Application\ Support/Code/User/snippets
+
+# ack
+ln -s ${BASEDIR}/ackrc ~/.ackrc
