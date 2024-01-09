4f792ed17147b0ee335dda2dbb241381b97fb323
Max Hill <max@hilloco.se>
2024-01-09 11:50:39 +0100

    chore: formatting

--- configs/nvim/.config/nvim/lua/user/snippets/init.lua
+++ configs/nvim/.config/nvim/lua/user/snippets/init.lua
@@ -13,7 +13,6 @@ ls.config.set_config {
 	enable_autosnippets = true,
 
 	-- Crazy highlights!!
-	-- #vid3
 	-- ext_opts = nil,
 	ext_opts = {
 		[types.choiceNode] = {
@@ -29,12 +28,12 @@ ls.config.set_config {
 -- s(context, nodes, condition, ...)
 local snippet = ls.s
 local fmt = require('luasnip.extras.fmt').fmt -- String and list of nodes
-local t = ls.text_node -- Create text node
-local i = ls.insert_node -- Location for the cursor to jump to
-local f = ls.function_node -- Takes a function and returns text
-local c = ls.choice_node -- Step through different choices
-local d = ls.dynamic_node -- Not sure...
-local same = function(index) -- Reuse an argument
+local t = ls.text_node                        -- Create text node
+local i = ls.insert_node                      -- Location for the cursor to jump to
+local f = ls.function_node                    -- Takes a function and returns text
+local c = ls.choice_node                      -- Step through different choices
+local d = ls.dynamic_node                     -- Not sure...
+local same = function(index)                  -- Reuse an argument
 	return f(function(args)
 		return args[1]
 	end, { index })
