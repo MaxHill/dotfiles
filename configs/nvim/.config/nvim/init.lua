17c5388d27b220bd6eb8f0128aba60af95ad44e8
Max Hill <max@hills.nu>
2025-08-09 09:37:16 +0200

    feat: LSP setup working

--- configs/nvim/.config/nvim/init.lua
+++ configs/nvim/.config/nvim/init.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+require("user.options");
 
 -- -----------------------------
 -- Plugins
@@ -8,10 +9,14 @@ vim.pack.add({
     { src = "https://github.com/vague2k/vague.nvim" },
     { src = "https://github.com/echasnovski/mini.pick" },
     { src = "https://github.com/echasnovski/mini.surround" },
+    { src = "https://github.com/echasnovski/mini.comment" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/Saghen/blink.cmp",                 version = '1.*' },
+    -- LSP
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/Saghen/blink.cmp",               version = '1.*' },
+    { src = "https://github.com/Hoffs/omnisharp-extended-lsp.nvim" },
+
 })
 -- vim.pack.update();
 
@@ -23,7 +28,7 @@ require "mini.pick".setup()
 -- -----------------------------
 require("nvim-treesitter.configs").setup({
     modules = {},
-    ensure_installed = { "svelte", "javascript", "lua" },
+    ensure_installed = { "lua", "c_sharp" },
     sync_install = false,
     auto_install = true,
     ignore_install = {},
@@ -34,60 +39,160 @@ require("nvim-treesitter.configs").setup({
 
 -- LSP
 -- -----------------------------
+require("omnisharp_extended")
 require("mason").setup()
+---@class LspConfig
+---@field mason_name? string Name of the LSP package in Mason
+---@field lsp_name string Name of the LSP server
+---@field config? table LSP configuration table
 
-vim.lsp.enable({
-    "lua_ls",
-    "typescript",
-    "emmetls",
-    "gopls",
-    "rust_analyzer",
-    "cssls",
-    "svelte",
-    "gleam",
-    "ts_ls",
-    "marksman",
-    "emmet_ls", -- TODO: settings
-    "omnisharp" -- TODO: settings
-})
--- TODO: Mason ensure_installed
+---@class Language
+---@field lsps? LspConfig[] List of LSP configurations for the
 
-
-vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true)
+---@type Language
+local lua = {
+    lsps = {
+        {
+            mason_name = "lua-language-server",
+            lsp_name = "lua_ls",
+            config = {
+                settings = {
+                    Lua = {
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true)
+                        }
+                    }
+                }
             }
         }
     }
+}
+
+---@type Language
+local csharp = {
+    lsps = {
+        {
+            mason_name = "omnisharp",
+            lsp_name = "omnisharp",
+            config = {
+                cmd = { "omnisharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+                root_markers = { "*.csproj", "*.sln", ".git" },
+                filetypes = { "cs" },
+                handlers = {
+                    ["textDocument/definition"] = function(...)
+                        return require("omnisharp_extended").definition_handler(...)
+                    end,
+                    ["textDocument/typeDefinition"] = function(...)
+                        return require("omnisharp_extended").type_definition_handler(...)
+                    end,
+                    ["textDocument/references"] = function(...)
+                        return require("omnisharp_extended").references_handler(...)
+                    end,
+                    ["textDocument/implementation"] = function(...)
+                        return require("omnisharp_extended").implementation_handler(...)
+                    end,
+                },
+                init_options = {
+                    useModernNet = true,
+                },
+                settings = {
+                    FormattingOptions = {
+                        EnableEditorConfigSupport = true,
+                        OrganizeImports = true,
+                    },
+                    MsBuild = {
+                        LoadProjectsOnDemand = false,
+                    },
+                    RoslynExtensionsOptions = {
+                        EnableAnalyzersSupport = true,
+                        EnableImportCompletion = true,
+                        AnalyzeOpenDocumentsOnly = false,
+                    },
+                    Sdk = {
+                        IncludePrereleases = true,
+                    }
+                }
+            }
+        }
+    }
+}
+
+---@type Language[]
+local languages = { lua, csharp }
+
+-- Global defaults for all servers
+vim.lsp.config('*', {
+    -- TODO: Remove csproj & sln here
+    root_markers = { '.git', 'package.json', '*.csproj', '*.sln' },
+    capabilities = require('blink.cmp').get_lsp_capabilities(),
+    on_attach = function(client, bufnr)
+        -- "grn" is mapped in Normal mode to vim.lsp.buf.rename()
+        -- "gra" is mapped in Normal and Visual mode to vim.lsp.buf.code_action()
+        -- "grr" is mapped in Normal mode to vim.lsp.buf.references()
+        -- "gri" is mapped in Normal mode to vim.lsp.buf.implementation()
+        -- "grt" is mapped in Normal mode to vim.lsp.buf.type_definition()
+        -- "gO" is mapped in Normal mode to vim.lsp.buf.document_symbol()
+        -- CTRL-S is mapped in Insert mode to vim.lsp.buf.signature_help()
+
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to definition" })
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "Hover documentation" })
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code action" })
+        vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "[G]et [L]ine diagnostics" })
+
+        -- Show when LSP attaches
+        print("LSP attached: " .. client.name)
+    end,
 })
+
+function mason_install(name)
+    if not require("mason-registry").is_installed(name) then
+        vim.cmd("MasonInstall " .. name)
+    end
+end
+
+-- Install and configure each language
+-- TODO: Make defensive
+for _, language in pairs(languages) do
+    if type(language.lsps) == "table" then
+        for _, lsp in pairs(language.lsps) or {} do
+            if lsp.config then
+                vim.lsp.config(lsp.lsp_name, lsp.config)
+            end
+
+            if lsp.mason_name then
+                mason_install(lsp.mason_name)
+            end
+
+            vim.lsp.enable(lsp.lsp_name)
+        end
+    end
+end
 
 -- Completion
 -- -----------------------------
 require('blink.cmp').setup({
-  keymap = {
-    preset = 'default',
-    -- Override any conflicting keymaps
-    ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-    ['<C-e>'] = { 'hide' },
-    ['<C-y>'] = { 'select_and_accept' },
-  },
-  appearance = {
-    use_nvim_cmp_as_default = false, -- Don't use nvim-cmp styling
-  },
-  completion = {
-    accept = {
-      auto_brackets = {
-        enabled = true,
-      },
+    keymap = {
+        preset = 'default',
+        -- Override any conflicting keymaps
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide' },
+        ['<C-y>'] = { 'select_and_accept' },
     },
-    menu = {
-      draw = {
-        treesitter = { "lsp" },
-      },
+    appearance = {
+        use_nvim_cmp_as_default = false, -- Don't use nvim-cmp styling
     },
-  },
+    completion = {
+        accept = {
+            auto_brackets = {
+                enabled = true,
+            },
+        },
+        menu = {
+            draw = {
+                treesitter = { "lsp" },
+            },
+        },
+    },
 })
 
 -- Disable built-in completion sources
@@ -96,10 +201,10 @@ vim.opt.complete = ''
 
 -- Disable omnifunc for all filetypes
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.opt_local.omnifunc = ""
-  end,
+    pattern = "*",
+    callback = function()
+        vim.opt_local.omnifunc = ""
+    end,
 })
 
 -- Colors
@@ -112,8 +217,6 @@ vim.cmd(":hi statusline guibg=NONE")
 -- Keymaps
 -- -----------------------------
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 -- Copy/cut
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without yank" })
@@ -133,7 +236,6 @@ vim.keymap.set("n", "<leader>rw", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center screen when going up" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center screen when going down" })
 vim.keymap.set("n", "n", " nzz", { desc = "Center screen when moving through results" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center screen when going down" })
 
 -- Lsp
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
